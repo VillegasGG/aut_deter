997ac6c9dc4f6ec0f81f82d40d74061fef444ccb
Get <villegas.get@gmail.com>
2024-10-14 21:29:55 -0600

    Validando q0

--- helper.py
+++ helper.py
@@ -31,16 +31,25 @@ def entrada():
             else:
                 print(f"Error: You must enter {len(alfabeto)} transitions separated by spaces. (\"-\" for empty)")
     
-    q0 = input("Enter the initial state: ")
+    q0 = leer_q0(estados)
     estados_aceptados = input("Enter the ACCEPTING states separated by a space: ").split(' ')
 
     G = grafo.crearGrafo(estados, alfabeto, matriz_transiciones, q0, estados_aceptados)
 
     return G, q0, alfabeto
 
+def leer_q0(estados):
+    while(True):
+        q0 = input("Enter the initial state: ")
+        if q0 not in estados:
+            print(f"Error: '{q0}' no está en la lista de estados {estados}. Inténtalo de nuevo.")
+        else:
+            return q0
+
+
 def leer_string(alfabeto):
     while(True):
-        cadena = input("Enter the string to validate (Separate each transition with spaces. E.g.: 0 1 1 1): ").split(' ')
+        cadena = input("Enter the string to validate (Separate each transition with spaces. E.g.: 0 1 1 1): ").strip().split(' ')
         b = True
         for el in cadena:
             if el not in alfabeto:
